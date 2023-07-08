97f7b3a8d32e102ed86dc85e3ed6904ac257a6db
Arely2409 <arely.aguilarfarias@utxicotepec.edu.mx>
2023-07-07 20:52:53 -0600

    DB_Actualizado

---
+++ Base_de_Datos/bd_nutrimads.sql
@@ -0,0 +1,137 @@
+CREATE DATABASE  IF NOT EXISTS `bd_nutrimads` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bd_nutrimads`;
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bd_nutrimads
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alimento`
+--
+
+DROP TABLE IF EXISTS `alimento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alimento` (
+  `ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(25) NOT NULL,
+  `Grupo_alimenticio` enum('Frutas','Verduras','Cereales','Leguminosas','Origen animal') NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alimento`
+--
+
+LOCK TABLES `alimento` WRITE;
+/*!40000 ALTER TABLE `alimento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alimento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `componente`
+--
+
+DROP TABLE IF EXISTS `componente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `componente` (
+  `ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Cantidad` float NOT NULL,
+  `Unidad_medida` enum('g','µg','mg','UI','cal','kcal','ml') NOT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `fk_ComponenteAlimento` FOREIGN KEY (`ID`) REFERENCES `alimento` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `componente`
+--
+
+LOCK TABLES `componente` WRITE;
+/*!40000 ALTER TABLE `componente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `componente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `criterio`
+--
+
+DROP TABLE IF EXISTS `criterio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `criterio` (
+  `ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `Estatus` enum('Saludable','No saludable') NOT NULL,
+  `Calificacion` float DEFAULT NULL,
+  `Unidad_medida` int DEFAULT NULL,
+  `Comentario` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `criterio`
+--
+
+LOCK TABLES `criterio` WRITE;
+/*!40000 ALTER TABLE `criterio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `criterio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `factor`
+--
+
+DROP TABLE IF EXISTS `factor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `factor` (
+  `ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Estatus` enum('Temprano','Estable','Grave') NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `factor`
+--
+
+LOCK TABLES `factor` WRITE;
+/*!40000 ALTER TABLE `factor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `factor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'bd_nutrimads'
+--
+
+--
+-- Dumping routines for database 'bd_nutrimads'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-07-07 20:48:46
